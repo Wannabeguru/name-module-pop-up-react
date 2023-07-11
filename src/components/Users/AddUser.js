355df4aeadfee1f8d989b838e13bcfab498b1802
Ophir Bentov <wannabegurux@gmail.com>
2023-07-11 11:07:08 -0700

    enabled fetching of name and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,31 @@
 import Card from '../UI/Card'
 import classes from './AddUser.module.css'
 import Button from '../UI/Button';
+import React, {useState} from 'react'
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
